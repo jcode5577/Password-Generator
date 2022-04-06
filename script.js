befc61783408e2c70986d5b7c84cc4e87f62b76d
jcode5577 <99107295+jcode5577@users.noreply.github.com>
2022-04-06 12:58:11 -0500

    final javaScript Touch ups

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Arrays for potential character choices
+var numbers = ['0','1','2','3','4','5','6','7','8','9']
+var characters=['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
+var lowerCase=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// Created Arrays of Possible Character Choices
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
-];
-
-// Created function to ask user which options and made conditional statements to ensure minimum option requirements were met.
-function questions() {
+//Create Prompts to ask user password criteria/ made a statement so that password criteria is met.
+  function questions() {
   var isValid = false;
   do {
-    var length = prompt("Choose password length between 8 and 128 characters");
-    var askNumbers = confirm("Do you want your password to include numbers?");
-    var askLowerCase = confirm("Do you want your password to include lower case letters?");
-    var askUpperCase = confirm("Do you want your password to include upper case letters?");
-    var askSpecial = confirm("Do you want your password to include special characters?");
-    var responses = {
-      length: length,
-      askNumbers: askNumbers,
-      askLowerCase: askLowerCase,
-      askUpperCase: askUpperCase,
-      askSpecial: askSpecial
-    } 
-    if((length < 8)||(length > 128))
-    alert("Choose number between 8 and 128");
-    else if((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askSpecial))
-    alert("Must choose at least one type.");
+  var length = prompt("Please select a password length between 8 and 128.")
+  var askNumbers= confirm("Do you wish to include any numbers?");
+  var askCharacters =confirm("Do you want to include and special characters?");
+  var askLowerCase = confirm("Do you want to include any lower case letters?");
+  var askUpperCase = confirm ("Do you want to include any upper case letters?");
+  var responses = {
+  length: length,
+  askNumbers: askNumbers,
+  askLowerCase: askLowerCase,
+  askUpperCase: askUpperCase,
+  askSpecial: askCharacters,
+} 
+  if ((length < 8)||(length > 128))
+  alert("Please select a password length between 8 and 128.")
+  else if((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askCharacters))
+  alert("Please choose atleast one type ie Upper case, Lower case, Number, Character.")
     else
-    isValid = true;
+  isValid= true;
 
   } while(!isValid);
-  return responses;
+return responses;
 }
-// This function joins all the user responses and then creates the result - a strong password.
-function generatePassword() {
-  var passwordOptions = questions();
-  var possibleCombo = [];
+// This function joins all the user responses and then creates the result
+function createPassword() {
+  var passwordOptions= questions();
+  var passwordCombo= [];
   var finalPassword = "";
-
-
 
   if (passwordOptions.askNumbers) {
     for (var i of numbers)
@@ -82,3 +78,5 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+  
