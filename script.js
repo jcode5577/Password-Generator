26122e03e9c04d18c6dc34aa1283182771ee7bf7
jcode5577 <99107295+jcode5577@users.noreply.github.com>
2022-04-06 11:41:24 -0500

    javascript setup

--- script.js
+++ script.js
@@ -1,74 +1,144 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//Creating password object.
+var pwdCriteria = {
+
+  //Property for length of password
+  pwdLength: 0,
+
+  //array to hold lowercase letters
+  pwdLowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+  //array to hold uppercase letters
+  pwdUpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+
+  //array to hold numbers
+  pwdNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+  //array to hold special characters
+  pwdCharacter: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+    "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]//32
+}
+
+// Write password to the #password input on index.html
+function writePassword() {
+  //call generatePassword function
+  var password = generatePassword();
   
-   var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   var abcUpperArr = abcUpper.split("");
-   var abcLower = "abcdefghijklmnopqrstuvwxyz";
-   var abcLowerArr = abcLower.split("");
-   var num = "0123456789";
-   var numArr = num.split("");
-   var sym = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
-   var symArr = sym.split("");
+  //set passwordText = to the textArea on index.html witht he ID of password
+  var passwordText = document.querySelector("#password");
 
-//  Prompt instruction for the user to begin
+  //update the textArea with the new password
+  passwordText.value = password;
+}
 
-window.onload = alert("Welcome! Please click 'Generate password' to start!");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-// Main function = Password Generation
+//function to handle the operations to generate a new password
+function generatePassword() {
 
-function generatePass(){
-    var allChars = [];
-    var resultPass = "";
+  //holds the password to be generated and returned 
+  var result = "";
 
-// Set dynamic variables
+  //variables to collect input from user prompts
+  var passwordLength = 0;
+  var upperCase;
+  var lowerCase;
+  var numbers;
+  var specialChar;
 
-    var Totlength = prompt("How many characters would you like your password to be?");
+  //initialize characters
+  passwordLength = 0;
+  pwdCriteria.pwdLength = 0;
+  result = "";
 
-    if(Totlength <8 || Totlength > 128){
-        alert("It is recommended to have a password between 8 and 128 characters long!\nPlease start over.");
+  //check password length
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("How many characters do you want your password to be? \nPassword must be between 8 and 128 characters.");
+
+    //if user presses cancel
+    if (passwordLength === null) {
+      return "Your secure password";
+    }
+    else {
+      //checking user enters an integer
+      if (!isFinite(passwordLength)) {
+        alert("You did not enter a number");
+        return "Your secure password";
+      }
+      else {
+        //check password meets length criteria
+        if (passwordLength < 8 || passwordLength > 128) {
+          alert("Password must be between 8 and 128 characters.");
+          return "Your secure password";
+        }
+        else {
+
+          //call the internal function to show prompts for criteria
+          showPrompts();
+
+          //keep adding characters based on criteria until pwdLength is = to the length the user set
+          while (pwdCriteria.pwdLength < passwordLength) {
+            //if statement to make sure the user selects at least one of the criteria  
+            if (lowerCase === false && upperCase === false && numbers === false && specialChar === false) {
+              alert("You must select at least one criteria of lowercase, uppercase, numbers or special characters")
+              showPrompts();
+            }
+            else {
+              //if the user selected lowercase and there is still room to add characters then
+              //randomly grab a lowercase letter from the array and add it to the end of result 
+              //update pwdLength by 1
+              if (lowerCase === true && pwdCriteria.pwdLength < passwordLength) {
+                var lc = pwdCriteria.pwdLowerCase[Math.floor(Math.random() * 26)]
+                result = result + lc;
+                pwdCriteria.pwdLength++;
+              }
+
+              //if the user selected a special character and there is still room to add characters then
+              //randomly grab a apecial character from the array and add it to the end of result 
+              //update pwdLength by 1              
+              if (specialChar === true && pwdCriteria.pwdLength < passwordLength) {
+                var sc = pwdCriteria.pwdCharacter[Math.floor(Math.random() * 32)]
+                result = result + sc;
+                pwdCriteria.pwdLength++;
+              }
+
+              //if the user selected an uppercase letter and there is still room to add characters then
+              //randomly grab an uppercase letter from the array and add it to the end of result 
+              //update pwdLength by 1
+              if (upperCase === true && pwdCriteria.pwdLength < passwordLength) {
+                var uc = pwdCriteria.pwdUpperCase[Math.floor(Math.random() * 26)]
+                result = result + uc;
+                pwdCriteria.pwdLength++;
+              }
+
+              //if the user selected a number and there is still room to add characters then
+              //randomly grab a number from the array and add it to the end of result 
+              //update pwdLength by 1
+              if (numbers === true && pwdCriteria.pwdLength < passwordLength) {
+                var num = pwdCriteria.pwdNumber[Math.floor(Math.random() * 10)]
+                result = result + num;
+                pwdCriteria.pwdLength++;
+              }
+            }
+          }
+        }
+      }
     }
 
-// Confirm the rest of character conditions
-    
-    else{
-        if(confirm("Would you like your password to contain upper case letters?")){
-            Array.prototype.push.apply(allChars, abcUpperArr);
-        }
+    //return the generated password back to the calling function
+    return result;
 
-        if(confirm("Would you like your password to contain lower case letters?")){
-            Array.prototype.push.apply(allChars, abcLowerArr);
-        }
-
-        if(confirm("Would you like your password to contain numbers?")){
-            Array.prototype.push.apply(allChars, numArr);
-        }
-
-        if(confirm("Would you like your password to contain symbols?")){
-            Array.prototype.push.apply(allChars, symArr);
-        }
-
-        if(allChars.length===0){
-            alert("You must select at lease 1 type of characters to generate a password!\nPlease start over.");
-        }
-
-// Run for loop to use confirmed information and generate password as a result
-
-        else{
-            for(var i=0; i<Totlength; i++){
-                var random = Math.floor(Math.random()*allChars.length);
-                resultPass += allChars[random];
-            }
-        }
-        }
-
-// Display the result
-
-        document.getElementById("password").innerHTML = resultPass;
-   }
-
-// Bonus! function to copy password to clipboard
-
-function copyPass(){
-
-    document.querySelector("textarea").select();
-    document.execCommand("Copy");
-    alert("Password copied to clipboard!");
+    //internal function to prompt the user for criteria
+    function showPrompts() {
+      lowerCase = confirm("Do you want to use lower case letters?");
+      upperCase = confirm("Do you want to use upper case letters?");
+      numbers = confirm("Do you want to use numbers?");
+      specialChar = confirm("Do you want to use any special characters?");
+    }
+  }
 }
